cfa1572b4bffb7e5a2a460925faf77d0cf0364bb
7akoda <114258910+7akoda@users.noreply.github.com>
2024-12-05 11:39:33 -0800

    made it so logo resizes on reload, not great

--- app/components/icons/Board/logos/moogboardlogo.jsx
+++ app/components/icons/Board/logos/moogboardlogo.jsx
@@ -4,19 +4,24 @@ const Mooglogo = () => {
   if (typeof document !== "undefined") {
     const svgElement = document.querySelector("svg");
 
-    window.addEventListener("scroll", () => {
+    const updateScale = () => {
       const scrollHeight = document.body.scrollHeight - window.innerHeight;
       const scrollPercent = window.scrollY / scrollHeight;
       const clampedPercent = Math.min(Math.max(scrollPercent, 0), 1);
       svgElement.style.transform = `scale(${1 - clampedPercent * 0.3})`;
-    });
+    };
+    window.addEventListener("load", updateScale);
+    window.addEventListener("scroll", updateScale);
+    updateScale();
   }
+
   return (
+    //multiple svgs with slight viewbox changes to account for discrepancies with hand modelled font in regards to overlapping opacity
     <>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 278.67 296.25"
-        className="flex flex-col justify-center max-h-[70vh] fixed center z-0"
+        className="flex flex-col justify-center max-h-[70vh] fixed z-0"
       >
         <defs>
           <style>{`
@@ -104,13 +109,82 @@ const Mooglogo = () => {
                   d="M155.08,158.6c.03-10.37-.03-20.73-.18-31.1-.15-12.35,5.03-19.28,14.32-18.73,9.26.59,15.23,7.94,15.8,19.75.43,9.97.61,19.91.53,29.86-.13,11.79-6,18.86-15.52,19.22-9.54.33-14.96-6.65-14.93-19ZM177.62,157.02c.04-9.13-.09-18.26-.4-27.41-.3-8.29-2.82-12.19-7.63-12.43-4.82-.24-6.98,3.5-6.81,11.96.19,9.32.27,18.63.25,27.95-.03,8.46,2.25,12.23,7.17,12.12,4.9-.12,7.37-3.91,7.42-12.19Z"
                 />
               </g>
-              <path
-                className="cls-0"
-                d="M57.74,173.41c0-.12.02-.24.01-.36-.98-13.83-.92-27.47.19-41.33.63-7.43,3.25-10.77,7.61-11.27,4.39-.5,6.22,2.47,5.71,10.27-.93,14.47-.98,28.8-.16,43.26,0,.13.04.25.06.38,2.59.17,5.18.33,7.77.47,0-.13.02-.25.02-.38-.73-14.78-.68-29.45.14-44.24.47-7.93,2.98-11.4,7.36-11.76,4.4-.36,6.34,2.83,5.99,11.03-.64,15.24-.68,30.41-.11,45.65,0,.14.04.27.05.4,2.6.12,5.19.22,7.79.32,0-.13.03-.26.03-.4-.48-15.93-.43-31.82.14-47.76.42-12.06-4.28-18.27-13.25-17.36-4.85.49-8.38,3.09-10.93,7.21-1.83-3.68-4.86-5.45-9.71-4.78-8.9,1.29-15.13,7.98-16.2,18.38-1.33,13.87-1.44,27.47-.33,41.3,0,.12.04.24.07.35,2.58.21,5.17.42,7.75.62Z"
-              />
+            </g>
+          </g>
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0.4 -3 278.67 296.25"
+          className="flex flex-col justify-center max-h-[49vh] fixed center z-0 opacity-50"
+        >
+          <defs>
+            <style>{`
+        .cls-0 {
+          fill: #FFFFFF;
+          stroke: #FFFFFF;
+          stroke-miterlimit: 10;
+          stroke-width: 3px;
+        }
+        .cls-2 {
+          fill: #231f20;
+          stroke-width: 0px;
+          
+        }
+      `}</style>
+          </defs>
+          <g id="Layer_1" data-name="Layer 1">
+            <circle
+              cx="139.36"
+              cy="146.91"
+              r="106.42"
+              fill="#b8cbcd"
+              fillOpacity={0}
+            />
+          </g>
+          <g id="Layer_2" data-name="Layer 2">
+            <g>
               <path
                 className="cls-0"
                 d="M210.5,112.7c-9.47-1.33-14.1,4.8-13.46,16.4.55,9.75.77,19.44.67,29.15-.13,11.56,4.95,17.85,14.68,17.07,2.64-.2,5.35-1.29,7.56-2.73-.19,2.69-.41,5.38-.68,8.09-.31,3.13-3.21,5.2-6.23,5.55-4.66.53-9.33,1.02-14,1.45-.1,0-.2.04-.3.06-.2,2.45-.42,4.91-.67,7.37.1,0,.19,0,.29,0,4.72-.16,9.43-.4,14.12-.71,7.58-1.12,12.59-6.27,13.49-14.03,2.17-16.54,2.68-32.56,1.47-48.89-1.05-10.37-7.54-17.42-16.95-18.78ZM220.6,156.61c-.09,6.92-3.75,10.87-7.77,11.09-5.12.27-7.26-3.13-7.18-10.91.07-8.59-.16-17.18-.69-25.81-.49-7.82,1.42-11.14,6.44-10.57,4.98.57,7.75,4.25,8.36,11.67.65,8.22.93,16.38.84,24.54Z"
+              />
+            </g>
+          </g>
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="-0.1 -3 278.67 296.25"
+          className="flex flex-col justify-center max-h-[49vh] fixed center z-0 opacity-50"
+        >
+          <defs>
+            <style>{`
+        .cls-0 {
+          fill: #FFFFFF;
+          stroke: #FFFFFF;
+          stroke-miterlimit: 10;
+          stroke-width: 3px;
+        }
+        .cls-2 {
+          fill: #231f20;
+          stroke-width: 0px;
+          
+        }
+      `}</style>
+          </defs>
+          <g id="Layer_1" data-name="Layer 1">
+            <circle
+              cx="139.36"
+              cy="146.91"
+              r="106.42"
+              fill="#b8cbcd"
+              fillOpacity={0}
+            />
+          </g>
+          <g id="Layer_2" data-name="Layer 2">
+            <g>
+              <path
+                className="cls-0"
+                d="M57.74,173.41c0-.12.02-.24.01-.36-.98-13.83-.92-27.47.19-41.33.63-7.43,3.25-10.77,7.61-11.27,4.39-.5,6.22,2.47,5.71,10.27-.93,14.47-.98,28.8-.16,43.26,0,.13.04.25.06.38,2.59.17,5.18.33,7.77.47,0-.13.02-.25.02-.38-.73-14.78-.68-29.45.14-44.24.47-7.93,2.98-11.4,7.36-11.76,4.4-.36,6.34,2.83,5.99,11.03-.64,15.24-.68,30.41-.11,45.65,0,.14.04.27.05.4,2.6.12,5.19.22,7.79.32,0-.13.03-.26.03-.4-.48-15.93-.43-31.82.14-47.76.42-12.06-4.28-18.27-13.25-17.36-4.85.49-8.38,3.09-10.93,7.21-1.83-3.68-4.86-5.45-9.71-4.78-8.9,1.29-15.13,7.98-16.2,18.38-1.33,13.87-1.44,27.47-.33,41.3,0,.12.04.24.07.35,2.58.21,5.17.42,7.75.62Z"
               />
             </g>
           </g>
