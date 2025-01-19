1691d751e033b7c693dd9e6f192aa8dac337d310
7akoda <114258910+7akoda@users.noreply.github.com>
2025-01-19 12:21:08 -0800

    overhaul to line, passed props, removed buttons

--- app/components/line.tsx
+++ app/components/line.tsx
@@ -1,73 +1,14 @@
 import React, { useState } from "react";
 
-const Line = ({}) => {
-  const [animate, setAnimate] = useState(false);
-  const [bpm, setBpm] = useState(7);
+interface lineProps {
+  animate: boolean;
 
-  const handleClick = () => {
-    setTimeout(() => setAnimate(true), 0);
-    setTimeout(
-      () => setAnimate(false),
-      bpm === 10 ? 9999 : bpm === 7 ? 6999 : bpm === 4 ? 3999 : 9999
-    );
-  };
+  bpm: number;
+}
 
+export const Line: React.FC<lineProps> = ({ bpm, animate }) => {
   return (
     <div className=" flex flex-col justify-start items-center ">
-      <button
-        disabled={animate}
-        className={animate ? "buttonPlaying" : "button"}
-        onClick={handleClick}
-      >
-        play
-      </button>
-      <div className="flex flex-row ">
-        <button
-          disabled={animate}
-          className={
-            bpm == 10 && animate
-              ? "buttonSMSelectedPlaying"
-              : bpm != 10 && animate
-              ? "buttonSMPlaying"
-              : bpm == 10 && !animate
-              ? "buttonSMSelected"
-              : "buttonSM"
-          }
-          onClick={() => setBpm(10)}
-        >
-          slow
-        </button>
-        <button
-          disabled={animate}
-          className={
-            bpm == 7 && animate
-              ? "buttonSMSelectedPlaying"
-              : bpm != 7 && animate
-              ? "buttonSMPlaying"
-              : bpm == 7 && !animate
-              ? "buttonSMSelected"
-              : "buttonSM"
-          }
-          onClick={() => setBpm(7)}
-        >
-          medium
-        </button>
-        <button
-          disabled={animate}
-          className={
-            bpm == 4 && animate
-              ? "buttonSMSelectedPlaying"
-              : bpm != 4 && animate
-              ? "buttonSMPlaying"
-              : bpm == 4 && !animate
-              ? "buttonSMSelected"
-              : "buttonSM"
-          }
-          onClick={() => setBpm(4)}
-        >
-          fast
-        </button>
-      </div>
       <svg
         viewBox="0 0 242.5 1.2"
         className={
@@ -88,5 +29,3 @@ const Line = ({}) => {
     </div>
   );
 };
-
-export default Line;
