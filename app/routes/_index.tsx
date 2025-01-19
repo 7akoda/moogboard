099211438e16606786f9eae2b45c3a0c9dad8b75
7akoda <114258910+7akoda@users.noreply.github.com>
2025-01-19 12:22:16 -0800

    now housing some state and related functions

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,6 +2,9 @@ import type { MetaFunction } from "@remix-run/node";
 import Board from "../components/board";
 import Mooglogo from "../components/icons/Board/logos/moogboardlogo";
 import PopUp from "~/components/popUp";
+import { useState } from "react";
+import { Line } from "../components/line";
+import { PopOut } from "~/components/popOut";
 export const meta: MetaFunction = () => {
   return [
     { title: "MoogBoard" },
@@ -10,6 +13,16 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const [animate, setAnimate] = useState(false);
+  const [bpm, setBpm] = useState(7);
+
+  const handleClick = () => {
+    setTimeout(() => setAnimate(true), 0);
+    setTimeout(
+      () => setAnimate(false),
+      bpm === 10 ? 9999 : bpm === 7 ? 6999 : bpm === 4 ? 3999 : 9999
+    );
+  };
   return (
     <>
       <div className="flex flex-col items-center ">
@@ -18,7 +31,19 @@ export default function Index() {
         </div>
         <div className="flex flex-col justify-center items-center w-screen h-[100dvh] max-w-[750px]">
           <Board></Board>
-          <PopUp></PopUp>
+          <Line bpm={bpm} animate={animate}></Line>
+          <PopUp
+            bpm={bpm}
+            setBpm={setBpm}
+            animate={animate}
+            handleClick={handleClick}
+          ></PopUp>
+          <PopOut
+            bpm={bpm}
+            setBpm={setBpm}
+            animate={animate}
+            handleClick={handleClick}
+          ></PopOut>
         </div>
       </div>
     </>
