35e28f40fc8ba010791dd88c795aee4e97143b40
7akoda <114258910+7akoda@users.noreply.github.com>
2025-01-24 12:26:42 -0800

    fix: length obstructed by sidebar

--- app/components/line.tsx
+++ app/components/line.tsx
@@ -8,7 +8,7 @@ interface lineProps {
 
 export const Line: React.FC<lineProps> = ({ bpm, animate }) => {
   return (
-    <div className=" flex flex-col justify-start items-center ">
+    <div className=" flex flex-col justify-start items-center  z-0 ">
       <svg
         viewBox="0 0 242.5 1.2"
         className={
@@ -23,7 +23,10 @@ export const Line: React.FC<lineProps> = ({ bpm, animate }) => {
         xmlns="http://www.w3.org/2000/svg"
       >
         <g>
-          <rect fill="#d97706" className="w-[100%] h-[0.5px]" />
+          <rect
+            fill="#d97706"
+            className="w-[80dvw] h-[0.5px] flex flex-row justify-self-center"
+          />
         </g>
       </svg>
     </div>
