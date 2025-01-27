85160810f09da34c1404b2d6432aec2df21ce4fe
7akoda <114258910+7akoda@users.noreply.github.com>
2025-01-27 15:37:57 -0800

    Feat: supabase backend

---
+++ app/lib/climbService.ts
@@ -0,0 +1,17 @@
+import { supabase } from "../lib/supabase";
+
+export async function fetchClimbs() {
+  const { data, error } = await supabase.from("climbs").select("*");
+  if (error) throw new Error(error.message);
+  return data;
+}
+
+export async function fetchClimbById(id: number) {
+  const { data, error } = await supabase
+    .from("Climbs")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) throw new Error(error.message);
+  return data;
+}
