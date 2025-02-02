6af4a1e726b478ccc8831a72506b8c86ec4c57d5
7akoda <114258910+7akoda@users.noreply.github.com>
2025-02-01 19:14:54 -0800

    added filtering feature

--- app/components/Board/search.tsx
+++ app/components/Board/search.tsx
@@ -23,21 +23,38 @@ export const Search = () => {
     getClimbs();
   }, []);
   return (
-    <table className="flex flex-col pt-6">
-      <thead>
-        <tr className="flex flex-row justify-between">
-          <th>Name</th>
-          <th>Grade</th>
-        </tr>
-      </thead>
-      <tbody>
-        {climbs?.map((climb) => (
-          <tr className="flex flex-row justify-between" key={climb.id}>
-            <td>{climb.name}</td>
-            <td>{climb.grade}</td>
+    <div className="">
+      <input
+        type="text"
+        placeholder="Search climbs..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <table className="flex flex-col pt-6">
+        <thead>
+          <tr className="flex flex-row justify-between">
+            <th>Name</th>
+            <th>Grade</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {climbs
+            ?.filter((climb) =>
+              climb.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((climb) => (
+              <tr className="flex flex-row justify-between" key={climb.id}>
+                <td>{climb.name}</td>
+                <td>{climb.grade}</td>
+              </tr>
+            )) ?? (
+            <tr>
+              <td colSpan={2}>No climbs available</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
