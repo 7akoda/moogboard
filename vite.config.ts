dbdb0d4def24c3720477afa9fe14138116672369
7akoda <114258910+7akoda@users.noreply.github.com>
2025-02-14 10:10:07 -0800

    configuring for SPA MODE

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/moogboard/",
   plugins: [
     remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
+      ssr: false,
     }),
-    tsconfigPaths(),
   ],
+  base: "/moogboard/",
 });
